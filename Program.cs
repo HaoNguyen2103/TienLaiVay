d1a55ff2ae200e991df9b9990e65bea2e5fe34f9
HaoNguyen2103 <nguyenhao21031995@gmail.com>
2025-08-11 20:21:15 +0700

    Money
    Lãi suất tiền vay

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        double money = 1;
+        int month = 1;
+        double interest_rate = 1.0;
+        Console.WriteLine("Nhap gia  tri money: ");
+        money = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Nhap so thang: ");
+        month = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Nhap so ty gia: ");
+        interest_rate = Convert.ToDouble(Console.ReadLine());
+        double total_interest = 0;
+        for (var i = 0; i < month; i++)
+        {
+            total_interest += money * (interest_rate / 100) / 12 * 3;
+
+        }
+        Console.WriteLine("Tong lai suat la: " + total_interest);
+    }
+}
